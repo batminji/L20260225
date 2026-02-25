dc9446cf461b8ede29433b6f0f89e2b5fb47bc6a
BATMINJI <minjiconan@naver.com>
2026-02-25 14:24:49 +0900

    Map에서 WASD키로 플레이어 움직임 구현

--- main.cpp
+++ main.cpp
@@ -16,13 +16,64 @@ int main()
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 
-	for (int Y = 0; Y < 10; ++Y)
-	{
-		for (int X = 0; X < 10; ++X)
+	int PlayerX = 1;
+	int PlayerY = 1;
+
+	while (true) {
+		for (int Y = 0; Y < 10; ++Y)
 		{
-			std::cout << Map[Y][X] << " ";
+			for (int X = 0; X < 10; ++X)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					std::cout << "% ";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					std::cout << "# ";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					std::cout << "  ";
+				}
+			}
+			std::cout << '\n';
 		}
-		std::cout << '\n';
+		int Temp = 0;
+		Temp = _getch();
+		if (Temp == 'W' || Temp == 'w')
+		{
+			--PlayerY;
+			if (PlayerY == 0)
+			{
+				++PlayerY;
+			}
+		}
+		else if (Temp == 'S' || Temp == 's')
+		{
+			++PlayerY;
+			if (PlayerY == 9)
+			{
+				--PlayerY;
+			}
+		}
+		else if (Temp == 'A' || Temp == 'a')
+		{
+			--PlayerX;
+			if (PlayerX == 0)
+			{
+				++PlayerX;
+			}
+		}
+		else if (Temp == 'D' || Temp == 'd')
+		{
+			++PlayerX;
+			if (PlayerX == 9)
+			{
+				--PlayerX;
+			}
+		}
+		system("cls");
 	}
 
 	return 0;
