119e0735a1ca11e2aeee6cf3d66a3d958df2a347
BATMINJI <minjiconan@naver.com>
2026-02-25 11:30:18 +0900

    print array

--- main.cpp
+++ main.cpp
@@ -2,21 +2,13 @@
 
 int main()
 {
-	int Num1 = 0;
-	int Num2 = 0;
+	int Array[10] = {};
+	
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << Array[i] << " ";
+	}
+	std::cout << std::endl;
 
-	// std::cin 같은 경우 한 번에 받지 말 것
-	std::cout << "첫 번째 숫자를 입력하세요 : ";
-	std::cin >> Num1;
-	std::cout << "두 번째 숫자를 입력하세요 : ";
-	std::cin >> Num2;
-
-	std::cout << Num1 << " + " << Num2 << " = " << Num1 + Num2 << '\n';
-	std::cout << Num1 << " - " << Num2 << " = " << Num1 - Num2 << '\n';
-	std::cout << Num1 << " * " << Num2 << " = " << Num1 * Num2 << '\n';
-	std::cout << Num1 << " / " << Num2 << " = " << Num1 / Num2 << '\n';
-	std::cout << Num1 << " % " << Num2 << " = " << Num1  % Num2 << '\n';
-
-	// 이것은 집에서 수정한 것이다!
 	return 0;
 }
