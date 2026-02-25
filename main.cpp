45d675e955ef2d87bc25e606b17389d4954abb64
BATMINJI <minjiconan@naver.com>
2026-02-25 13:09:57 +0900

    나머지 연산자를 쓰지 않은 최적화 방법
    for문도 절반으로 줄이고, 나머지 연산자 조건문 또한 검사하지 않기 때문에 더 빠르다.

--- main.cpp
+++ main.cpp
@@ -2,32 +2,13 @@
 
 int main()
 {
-	int SumResult = 0;
-	int EvenSumResult = 0;
-	int OddSumResult = 0;
-	int ThreeMultipleSumResult = 0;
+	int Total = 0;
 
-	for (int i = 1; i < 101; ++i)
+	for (int i = 1; i <= 100; i += 2)
 	{
-		SumResult += i;
-		if (i % 2 == 0)
-		{
-			EvenSumResult += i;
-		}
-		else
-		{
-			OddSumResult += i;
-		}
-
-		if (i % 3 == 0)
-		{
-			ThreeMultipleSumResult += i;
-		}
+		Total += i;
 	}
 
-	std::cout << "1부터 100까지의 합 : " << SumResult << '\n';
-	std::cout << "1부터 100 사이 짝수들의 합 : " << EvenSumResult << '\n';
-	std::cout << "1부터 100 사이 홀수들의 합 : " << OddSumResult << '\n';
-	std::cout << "1부터 100 사이 3의 배수의 합 : " << ThreeMultipleSumResult << '\n';
+	std::cout << Total << '\n';
 	return 0;
 }
